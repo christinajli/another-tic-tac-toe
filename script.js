22cdf6a4ccbe85581e03db02eddb8dd163e03238
christinajli <28367784+christinajli@users.noreply.github.com>
2022-10-25 23:30:14 -0400

    drag and drop pieces on board

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const RED_CLASS = 'red'
 const BLUE_CLASS = 'blue'
 const WINNING_COMBINATION = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
 let redTurn
-const fill = document.querySelector('.fill');
-const empties = document.querySelectorAll('.cell');
+const pieceElements = document.querySelectorAll('.piece');
+const cellElements = document.querySelectorAll('.cell');
+const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winning-message')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 const restartButton = document.getElementById('restart-button')
@@ -21,59 +22,74 @@ startGame()
 
 restartButton.addEventListener('click', startGame)
 
-// Fill listeners
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
+// Loop through pieces and add listeners
+for (const piece of pieceElements) {
+  piece.addEventListener('dragstart', dragStart);
+  piece.addEventListener('dragend', dragEnd);
+}
 
-// Loop through empty boxes and add listeners
-for (const empty of empties) {
-  empty.addEventListener('dragover', dragOver);
-  empty.addEventListener('dragenter', dragEnter);
-  empty.addEventListener('dragleave', dragLeave);
-  empty.addEventListener('drop', dragDrop);
+// Loop through empty cells and add listeners
+for (const cell of cellElements) {
+  cell.addEventListener('dragover', dragOver);
+  cell.addEventListener('dragenter', dragEnter);
+  cell.addEventListener('dragleave', dragLeave);
+  cell.addEventListener('drop', dragDrop);
 }
 
 function startGame() {
-    redTurn = true
-    //reset to initial board
-    winningMessageElement.classList.remove('show')
+  redTurn = true
+  //reset to initial board
+  winningMessageElement.classList.remove('show')
 }
 
-// Drag Functions
+// Reference on item being dragged
+let dragged
 
-function dragStart() {
-  this.className += ' hold';
-  setTimeout(() => (this.className = 'invisible'), 0);
+// Drag functions for pieces
+function dragStart(e) {
+  dragged = e.target;
+  e.target.classList.add('hold');
+  e.target.classList.add('dragging');
 }
 
-function dragEnd() {
-  this.className = 'fill';
+function dragEnd(e) {
+  e.target.classList.remove('hold');
+  e.target.classList.remove('dragging');
 }
 
+// Drag functions for board
 function dragOver(e) {
   e.preventDefault();
 }
 
 function dragEnter(e) {
   e.preventDefault();
-  this.className += ' hovered';
+  if (e.target.classList.contains('dropAllowed')) {
+    e.target.classList.add('hovering');
+  }
 }
 
-function dragLeave() {
-  this.className = 'cell';
+function dragLeave(e) {
+  if (e.target.classList.contains('dropAllowed')) {
+    e.target.classList.remove('hovering');
+  }
 }
 
-function dragDrop() {
-  this.className = 'cell';
-  this.append(fill);
-  endGame(false);
+function dragDrop(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('dropAllowed')) {
+    e.target.classList.remove('hovering');
+    e.target.classList.remove('dropAllowed');
+    dragged.parentNode.removeChild(dragged);
+    e.target.appendChild(dragged);
+  }
 }
 
 function endGame(draw) {
-    if (draw) {
-        winningMessageTextElement.innerText = 'Draw!'
-    } else {
-        winningMessageTextElement.innerText = `${redTurn ? "Red" : "Blue"} Wins!`
-    }
-    winningMessageElement.classList.add('show')
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${redTurn ? "Red" : "Blue"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
 }
